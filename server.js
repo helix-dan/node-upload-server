befa826e90e18fcf8366429a05b14fc0a9c34948
helix-dan <lotus.bat@gmail.com>
2013-01-24 17:36:27 +0800

    package for server.js

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 var http = require('http');
 
-var http_server = http.createServer(function(request, response){
-	response.writeHead(200);
-	response.write('Get it!');
-	response.end();
-});
+function start(){
+	function OnRequest(request, response){
+		response.writeHead(200);
+		response.write('Get it!');
+		response.end();
+	}
 
-http_server.listen(3104);
 
-console.log("localhost start on 3104");
+	http.createServer(OnRequest).listen(3104);
+	console.log("localhost start on 3104");
+}
+
+exports.start = start;
